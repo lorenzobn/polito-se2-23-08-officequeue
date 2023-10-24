4ccbb5be53af7ca7739402c8d239b67ec8644c87
Rick <riccardo.vukovic@gmail.com>
2023-10-24 10:33:08 +0200

    creation of the call customer front end part (status page)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton, Table } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
@@ -7,7 +7,6 @@ function App() {
   const [content, setContent] = useState('');
   const [ticketBooking, setTicketBooking] = useState({});
   
-
   function Main() {
     const navigate = useNavigate();
 
@@ -49,10 +48,14 @@ function App() {
   }
 
   function Ticket(){
+    const navigate = useNavigate();
     return (
       <>
       <h3>Great!</h3>
       <h5>Your ticket number for the service {ticketBooking.serviceType} is: {ticketBooking.number} </h5>
+      <div className='my-5'>
+        <Button variant='primary' onClick={() => navigate('/status')}>SHOW QUEUE STATUS</Button> 
+      </div>
       </>
     )
   }
@@ -84,11 +87,59 @@ function App() {
     );
   }
 
+  function Status() {
+    const navigate = useNavigate();
+
+    return (
+      <>
+        <Table className='table-striped' style={{ tableLayout: 'fixed', }}>
+          <thead>
+            <tr>
+              <th>DESK</th>
+              <th>SERVICE</th>
+              <th>CURRENT CUSTOMER</th>
+              <th>NEXT CUSTOMER</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>d1</td>
+              <td>A</td>
+              <td>9</td>
+              <td>10</td>
+              <td><Button>CALL NEXT</Button></td>
+            </tr>
+            <tr>
+              <td>d2</td>
+              <td>B</td>
+              <td>12</td>
+              <td>13</td>
+              <td><Button>CALL NEXT</Button></td>
+            </tr>
+            <tr>
+              <td>d3</td>
+              <td>C</td>
+              <td>14</td>
+              <td>15</td>
+              <td><Button>CALL NEXT</Button></td>
+            </tr>
+          </tbody>
+        </Table> 
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" className="btn btn-lg btn-primary fixed-right-bottom" viewBox="0 0 16 16" onClick={() => navigate('/')}>
+          <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+        </svg>
+        </div>
+      </>
+    )
+  } 
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<Main></Main>}></Route>
         <Route path={'/ticket'} element={<ShowTicket></ShowTicket>}></Route>
+        <Route path={'/status'} element={<Status></Status>}></Route>
       </Routes>
     </BrowserRouter>
   )
