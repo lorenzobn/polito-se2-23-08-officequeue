6d3a37823a4c782f138c50683d5984fc60eeb138
RiccardoVukovic320029 <s320039@studenti.polito.it>
2023-10-26 16:37:24 +0200

    modified api file

--- client/src/API.jsx
+++ client/src/API.jsx
@@ -2,7 +2,7 @@ const URL = 'http://localhost:3000/api/v1.0';
 
 async function getServices() {
 
-    const response = await fetch(URL + `/service-types/services`);
+    const response = await fetch(URL + `/service-types`);
     const services = await response.json();
     if (response.ok) {
         return Object.assign({}, services);
